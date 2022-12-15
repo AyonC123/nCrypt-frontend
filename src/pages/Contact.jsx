b37201836a6b669458f25664ac5b1593630fe76f
AyonC123 <106944474+AyonC123@users.noreply.github.com>
2022-12-15 09:27:39 +0000

    add contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,42 @@
+import { useState } from 'react'
+
 function Contact() {
+  const [email, setEmail] = useState("")
+  const [content, setContent] = useState("")
+
   return (
     <>
-      <h1 class="text-center text-lg">Home</h1>
+      <h1 className="text-center text-lg">Signup</h1>
+      <h3>support@exla.net</h3>
+      <form onSubmit={signup}>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.value)}
+            required
+          />
+        </div>
+        <div class="input-group">
+          <label for="content">Content</label>
+          <textarea
+            name="content"
+            id="content"
+            cols="30"
+            rows="10"
+            placeholder="Content"
+            value={content}
+            onChange={(e) => setEmail(e.value)}
+          ></textarea>
+        </div>
+        <div className="input-group">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </>
   );
 }
